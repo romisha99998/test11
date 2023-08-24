c91b52e3f8bce5e2fb58b40cc64aa8cbc8274e95
romisha99998 <romishagurung61@gmail.com>
2023-08-24 13:30:44 +0545

    august 23

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,65 @@
 import 'package:flutter/material.dart';
 void main(){
-  runApp(MaterialApp(home: MyApp()));
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: MyApp(),
+  ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var count= 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text('My App'),
-      centerTitle:true,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text(
+              'The count is :',
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.blueAccent
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              count.toString(),
+              style: TextStyle(
+                fontSize: 50,
+              ),
+            ),
+          ),
+        ],
       ),
+      appBar: AppBar(
+        leading:const Icon(Icons.menu) ,
+        title: const Text('My App'),
+        centerTitle: true,
+        actions: const [
+          Icon(Icons.search),
+          Icon(Icons.more_vert),
+        ],
+      ),
+      floatingActionButton:FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          setState((){
+            count++;
+          });
+        },
+      ) ,
     );
   }
-}
+  }
